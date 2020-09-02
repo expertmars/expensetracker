f8062dfe6b9c3a46541900fcec9f3e0e5aea4329
expertmars <expertmars@gmail.com>
2020-09-02 23:32:30 +0530

    listtile added

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -33,47 +33,30 @@ class TransactionList extends StatelessWidget {
           : ListView.builder(
               itemCount: userTransactions.length,
               itemBuilder: (context, index) {
-                return Card(
-                  child: Row(
-                    children: [
-                      Container(
-                        margin: EdgeInsets.symmetric(
-                          horizontal: 10,
-                          vertical: 10,
-                        ),
-                        padding: EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                          style: BorderStyle.solid,
-                          color: Theme.of(context).primaryColor,
-                          width: 2,
-                        )),
-                        child: Text(
-                          "\$${userTransactions[index].amount.toStringAsFixed(2)}",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Theme.of(context).primaryColor,
-                            fontSize: 20,
-                          ),
+                return Container(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 8,
+                    vertical: 6,
+                  ),
+                  child: Card(
+                    elevation: 6,
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        radius: 30,
+                        child: Padding(
+                          padding: const EdgeInsets.all(6.0),
+                          child: FittedBox(
+                              child: Text(
+                                  "\$${userTransactions[index].amount.toString()}")),
                         ),
                       ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            userTransactions[index].title,
-                            style: Theme.of(context).textTheme.headline6,
-                          ),
-                          Text(
-                            DateFormat.yMMMd()
-                                .format(userTransactions[index].date),
-                            style: TextStyle(
-                              color: Colors.grey,
-                            ),
-                          ),
-                        ],
+                      title: Text(
+                        userTransactions[index].title,
+                        style: Theme.of(context).textTheme.headline6,
                       ),
-                    ],
+                      subtitle: Text(DateFormat.yMMMd()
+                          .format(userTransactions[index].date)),
+                    ),
                   ),
                 );
               },
